1ad03325e042d3996483af595514b26e6e778a32
Ethan Harmon <ethanharmon2018@gmail.com>
2024-12-30 21:56:12 +0000

    Began writing logic for dotfiles installation

--- cmd/install.go
+++ cmd/install.go
@@ -6,7 +6,13 @@ package cmd
 import (
 	// "errors"
 	"fmt"
+	"io/fs"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
 
+	"github.com/go-git/go-git/v5"
 	"github.com/spf13/cobra"
 )
 
@@ -20,7 +26,75 @@ var installCmd = &cobra.Command{
 	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("install called")
+		user, err := user.Current()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		dotcomfy_path := user.HomeDir + ".dotcomfy"
+		home_dir := user.HomeDir
+
+		if len(args) > 1 {
+			fmt.Println("Too many arguments")
+			os.Exit(1)
+		}
+
+		temp_dir, err := os.MkdirTemp("", "dotcomfy-clone")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "DEBUGPRINT[3]: install.go:63: err=%+v\n", err)
+			os.Exit(1)
+		}
+
+		defer os.RemoveAll(temp_dir)
+
+		if strings.Contains(args[0], "dotfiles.git") {
+			fmt.Println("Custom repo")
+			_, err = git.PlainClone(temp_dir, false, &git.CloneOptions{
+				URL: args[0],
+			})
+
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "DEBUGPRINT[2]: install.go:56: err=%+v\n", err)
+				os.Exit(1)
+			}
+		} else {
+			fmt.Println("Username")
+			url := fmt.Sprintf("https://github.com/%s/dotfiles.git", args[0])
+			_, err = git.PlainClone(temp_dir, false, &git.CloneOptions{
+				URL: url,
+			})
+			fmt.Fprintf(os.Stderr, "DEBUGPRINT[4]: install.go:70: err=%+v\n", err)
+
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+
+		// Walk through the cloned repo and perform rename/symlink operations
+		err = filepath.WalkDir(dotcomfy_path, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "DEBUGPRINT[5]: install.go:84: err=%+v\n", err)
+				return err
+			}
+
+			if !d.IsDir() {
+				if strings.Contains(path, ".git") {
+					fmt.Println("Skipping .git directory")
+				} else if strings.Contains(path, temp_dir+"README.md") {
+					fmt.Println("Skipping root level README.md")
+				} else {
+
+				}
+			}
+			return nil
+		})
 	},
+}
+
+func rename_symlink_unix(old_dotfiles_path, dotcomfy_path, new_path string) string {
+	// TODO: implement this
+	return ""
 }
 
 func init() {
